2dae265587f0c785237d2b13ab8b771d6f8a594f
ShiveshJha12 <shubjha12@gmail.com>
2024-10-03 16:07:43 -0400

    new ip

--- components/RegionSelector.js
+++ components/RegionSelector.js
@@ -58,7 +58,7 @@ const RegionSelector = React.memo(({
 
             setIsLoading(true);
 
-            fetch(`http://3.86.139.48:5000/download-data?${requestParams.toString()}`)
+            fetch(`http://107.21.74.68:5000/download-data?${requestParams.toString()}`)
                 .then(response => {
                     if (!response.ok) {
                         throw new Error('Network response was not ok');
